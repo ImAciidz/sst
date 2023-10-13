24f3f63df8bbb9226a38dc1942fe3913d01edeaa
Hayden K <imaciidz@gmail.com>
2023-10-12 20:36:28 -0400

    Denote custom build of SST

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define NAME "SST"
 #define LONGNAME "Source Speedrun Tools Beta"
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 6
-#define VERSION "0.6"
+#define VERSION_MINOR 5
+#define VERSION "0.5 - For L4D2CE Original Campaigns Infinite Votes"
